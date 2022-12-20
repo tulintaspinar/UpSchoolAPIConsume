2b86dc3facaf75fb6c6bf4ec78754c5b0e1ab5c6
Tülin Taşpınar <tulinttaspinar@gmail.com>
2022-12-20 14:17:00 +0300

    Add project files.

---
+++ UpSchoolAPIConsume/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UpSchoolAPIConsume.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync("https://localhost:44375/api/Category");
+			if(response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+
+				return Ok(response);
+			}
+			return View();
+		}
+	}
+}
---
+++ UpSchoolAPIConsume/Models/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UpSchoolAPIConsume.Models
+{
+	public class CategoryViewModel
+	{
+		public int categoryID { get; set; }
+		public string categoryName { get; set; }
+		public string description { get; set; }
+		public bool status { get; set; }
+	}
+}
